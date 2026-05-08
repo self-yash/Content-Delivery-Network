6394a478af4312349157e1b70e6bb0e4f688edbc
self-yash <yashmehta0005@gmail.com>
2026-05-08 13:10:15 +0530

    created service model

--- image/models.py
+++ image/models.py
@@ -7,3 +7,21 @@ def upload_path(instance, filename):
 
 class ImageModel(models.Model):
     image = models.ImageField(upload_to=upload_path)
+
+class Service(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    api_key = models.CharField(
+        max_length=255,
+        unique=True,
+        default=uuid.uuid4
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'services'
