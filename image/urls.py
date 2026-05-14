bce9fd46b22ac61d0a51f6b30dda3451863ddecb
self-yash <yashmehta0005@gmail.com>
2026-05-14 21:22:29 +0530

    linked its url and tested

--- image/urls.py
+++ image/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register_service, upload_image
+from .views import *
 
 app_name = 'image'
 
 urlpatterns = [
     path('register/', register_service),
     path('upload/', upload_image),
+    path('regenerate-key/', regenerate_api_key),
 ]
