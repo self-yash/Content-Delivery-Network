8b073bf9f0927578d8660a220421fa9845918b47
self-yash <yashmehta0005@gmail.com>
2026-05-17 17:59:33 +0530

    added swagger

--- cdn/urls.py
+++ cdn/urls.py
@@ -16,8 +16,41 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="CDN API",
+      default_version='v1',
+      description="Content Delivery Network API",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('image/', include('image.urls')),
+
+    # swagger
+    path(
+        'swagger/',
+        schema_view.with_ui(
+            'swagger',
+            cache_timeout=0
+        ),
+        name='schema-swagger-ui'
+    ),
+
+    # redoc
+    path(
+        'redoc/',
+        schema_view.with_ui(
+            'redoc',
+            cache_timeout=0
+        ),
+        name='schema-redoc'
+    ),
 ]
